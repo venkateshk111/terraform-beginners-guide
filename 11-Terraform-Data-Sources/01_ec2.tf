c8ce95c8f36915e2f04fd24567aca430239ddd0c
Venkatesh <Venkatesh111@gmail.com>
2024-01-28 18:12:01 +0530

    terrform fmt

--- 11-Terraform-Data-Sources/01_ec2.tf
+++ 11-Terraform-Data-Sources/01_ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "myec2" {
-  ami = data.aws_ami.amzn_linux_2023_latest.id # fetching Latest Amazon Linux AMI id from data sources
+  ami           = data.aws_ami.amzn_linux_2023_latest.id # fetching Latest Amazon Linux AMI id from data sources
   instance_type = var.ec2_instance_type
 
   tags = {
