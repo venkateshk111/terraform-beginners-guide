96d5f8d2094a862c67b704b5304861c3d7cdb269
Venkatesh <Venkatesh111@gmail.com>
2024-04-01 00:02:22 +0530

    update s3 code

--- 99-Terraform-Example-Codes/02-s3-static-website/02_s3.tf
+++ 99-Terraform-Example-Codes/02-s3-static-website/02_s3.tf
@@ -1,6 +1,7 @@
 # Step 1: Create a bucket
 resource "aws_s3_bucket" "mybucket" {
-  bucket = var.bucket_name
+  bucket        = var.bucket_name
+  force_destroy = true
 }
 
 # Step 2: Enable static website hosting
@@ -47,7 +48,7 @@ resource "aws_s3_object" "index" {
   bucket       = aws_s3_bucket.mybucket.bucket
   key          = "index.html"
   source       = "website-files/index.html"
-  content_type = "text/html" 
+  content_type = "text/html"
 }
 
 #Step 6: Configure an error document
@@ -55,5 +56,5 @@ resource "aws_s3_object" "error" {
   bucket       = aws_s3_bucket.mybucket.bucket
   key          = "error.html"
   source       = "website-files/error.html"
-  content_type = "text/html" 
+  content_type = "text/html"
 }
